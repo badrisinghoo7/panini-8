092db74aa037bcae4ba895680fdebdba3b62bc3d
Badri vishal singh <badri.singh8090@gmail.com>
2025-04-25 12:18:24 +0530

    adding user functionality

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -32,3 +32,67 @@ export const userLogin = async (req, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   res.status(200).json({ message: "Login successful", token });
 };
+
+// get all users
+export const getUser = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// get user by id
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// get user profile by id
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// update user profile by id
+
+export const updateUserById = async (req, res) => {
+  try {
+    const { username, email } = req.body;
+    const id = req.params.id;
+    const userId = req.user.userId;
+    if (id !== userId) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to update this user" });
+    }
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { username, email },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { userRegister, userLogin } from "../controller/user.controller.js";
+import { userRegister, userLogin, getUser, getUserById, updateUserById } from "../controller/user.controller.js";
+import auth from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", userRegister);
 userRouter.post("/login", userLogin);
-
+userRouter.get("/getUsers", getUser);
+userRouter.get("/getUser/:id", auth,getUserById);
+userRouter.put("/updateUser/:id",auth, updateUserById);
 export default userRouter;
