8f919bb92a169cb2a2b79d3e455604f851ef2e60
Badri vishal singh <badri.singh8090@gmail.com>
2025-04-29 11:37:28 +0530

    adding

--- frontent/paneni/src/App.jsx
+++ frontent/paneni/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Profile from "./Components/Profile";
+import Blogs from "./Components/Blogs";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          toastStyle={{top: "50px"}}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+        />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontent/paneni/src/constants/api.js
@@ -0,0 +1,2 @@
+export const api = `https://panini-8.onrender.com`;
+// export const api = `http://localhost:3000/api`;
